0095cb636a86c54a2a16ef6d6ae553656b5ff535
Simon Lee <great.simonlee@gmail.com>
2022-10-03 14:08:45 -0400

    DB setting has been modified

--- wp-config.php
+++ wp-config.php
@@ -20,23 +20,17 @@
  */
 
 // ** Database settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
-
-/** Database username */
-define( 'DB_USER', 'root' );
-
-/** Database password */
-define( 'DB_PASSWORD', 'root' );
-
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
-
-/** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
-
-/** The database collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+if (strstr($_SERVER['SERVER_NAME'], 'chinamovesusa.local')) {
+	define( 'DB_NAME', 'local' );
+	define( 'DB_USER', 'root' );
+	define( 'DB_PASSWORD', 'root' );
+	define( 'DB_HOST', 'localhost' );
+} else {
+	define( 'DB_NAME', 'koreamov_chinamovesusa' );
+	define( 'DB_USER', 'koreamov_simon' );
+	define( 'DB_PASSWORD', '123qwe' );
+	define( 'DB_HOST', '127.0.0.1' );
+}
 
 /**#@+
  * Authentication unique keys and salts.
