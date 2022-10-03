3631c629fa864f59f506fec4c91c6ccda88d615b
Simon Lee <great.simonlee@gmail.com>
2022-10-03 17:43:03 -0400

    Rent page modified

--- wp-content/themes/bostonmoves/page-rent.php
+++ wp-content/themes/bostonmoves/page-rent.php
@@ -1,9 +1,4 @@
-<?php 
-    while(have_posts()) {
-        the_post(); ?>
-
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
-
-<?php }
-?>
+<?php get_header(); ?>
+<h1>THIS IS RENT PAGE</h1>
+<?php the_content(); ?>
+<?php get_footer(); ?>
