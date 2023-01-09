fac1535b137e8950fd584625434e259d6e3cef1d
Simon Lee <misaeng.dev@gmail.com>
2023-01-09 14:23:11 -0500

    About us page has been built

--- wp-content/themes/bostonmoves/build/index.asset.php
+++ wp-content/themes/bostonmoves/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '646d65ecaecd3db98192');
+<?php return array('dependencies' => array(), 'version' => 'af09ab8cce6ed93adc15');
--- wp-content/themes/bostonmoves/page-about-us.php
+++ wp-content/themes/bostonmoves/page-about-us.php
@@ -1,3 +1,61 @@
 <?php get_header(); ?>
-<h1>THIS IS ABOUT US PAGE.</h1>
+
+
+    <div class="aboutus-page-header">
+        <img class="aboutus-page-header-img" src="<?php echo get_theme_file_uri('assets/img/wallpaper/BMR-ABOUT-WALLPAPER.png') ?>" alt="ABOUTUS-WALLPAPER">
+        <div class="aboutus-page-header-div">
+            <div class="aboutus-page-header-text">
+                <p class="aboutus-page-header-title">MAKE YOUR BEST MOVE</p>
+                
+            </div>
+        </div>
+    </div>
+    <div class="aboutus-page-body">
+        <div class="aboutus-page-body-content">
+        
+            <div class="aboutus-main">
+                <div class="aboutus-main-catch">
+                    <p>At Boston Moves Real Estate, we operate as a small agency with the outlook of a much larger enterprise, providing the efficiency and professionalism one would expect from a major conglomerate with the personal touch of a brick and mortar shop.</p>
+                </div>
+                <hr style="width: 90%; max-width: 840px;"/>
+                <div class="aboutus-main-content">
+                    <p>Boston Moves Real Estate is built on a client-first mentality, acting as a small but highly efficient brokerage to meet your needs in an ever-shifting marketplace. Let’s face it - there’s nothing easy about moving, but our team of gifted agents work tirelessly to streamline what can otherwise be a chaotic process.</p>
+                </div>
+                <div class="aboutus-main-content">
+                    <p>Every Bostonian has a dream apartment, and at Boston Moves, our goal is to make that dream a reality - all the while bringing a smile to your face and saving you a few extra bucks in the interim.</p>
+                </div>
+                <div class="aboutus-main-content">
+                    <p>In a crowded field of brokerages scattered across the Greater Boston Area, Boston Moves stands apart.  Our staff is made up of the industry’s best, and between our mighty group of 15+, we have accumulated nearly 100 years of experience in the field.</p>
+                </div>
+            </div>
+            <div class="aboutus-divider">
+                <p>" Nothing brings a smile to our face quite like the act of finding our clients their dream home, and it would be our pleasure to do just that for you. "</p>
+            </div>
+            <div class="aboutus-join-contact-container">
+                <div class="aboutus-join-team">
+                    <p>JOIN OUR TEAM</p>
+                    <div class="aboutus-join-team-content">
+                        <p>Boston Moves Real Estate is a real estate brokerage with properties throughout the entire Greater Boston area. Among our services are: sales and leasing, condominium and investment sales, and roommate matching. Contact us today!</p>
+                    </div>
+                </div>
+                <div class="aboutus-contact-us">
+                    <p>CONTACT US</p>
+                    <div class="aboutus-contact-us-info">
+                        <p>manager@bostonmovesre.com</p>
+                        <p>475 Commonwealth Ave #103, Boston, MA 02215</p>
+                        <ul class="aboutus-contact-sns">
+                            <li class="aboutus-contact-sns-item"><a href="https://www.facebook.com/newyorkmovesre/" target='_blank'><i class="fa fa-facebook header-social-facebook"></i></a></li>
+                            <li class="aboutus-contact-sns-item"><a href="https://www.instagram.com/newyorkmovesteam/?hl=en" target='_blank'><i class="header-social-instagram fa fa-instagram"></i></a></li>
+                            <li class="aboutus-contact-sns-item"><a href="https://www.linkedin.com/company/new-york-moves" target='_blank'><i class="header-social-linkedin fa fa-linkedin"></i></a></li>
+                        </ul>
+                        <?php the_content(); ?>
+                    </div>
+                </div>
+            </div>
+        <img class="aboutus-location-img" src="<?php echo get_theme_file_uri('assets/img/wallpaper/BMR-ABOUT-MAP.png') ?>" alt="ABOUTUS-MAP">
+            
+            
+        </div>        
+    </div>
+
 <?php get_footer(); ?>
