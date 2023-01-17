d11d0760d9658344e14098a80fda904e86785d61
Simon Lee <misaeng.dev@gmail.com>
2023-01-17 12:51:25 -0500

    mod: background color removed

--- wp-content/themes/bostonmoves/page-buy.php
+++ wp-content/themes/bostonmoves/page-buy.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <!-- <h1>THIS IS BUY PAGE.</h1> -->
   <div style="display: flex; justify-content: center;">
-    <div style=" width: 85vw; background-color: tomato; ">
+    <div style=" width: 85vw;">
       <?php the_content(); ?>
     </div>
   </div>
