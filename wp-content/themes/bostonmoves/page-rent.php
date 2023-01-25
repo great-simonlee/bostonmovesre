acb615f4e46286094f6aa066c8cc85d8ad705ad1
Simon Lee <misaeng.dev@gmail.com>
2023-01-25 10:38:11 -0500

    fix: rent: center

--- wp-content/themes/bostonmoves/page-rent.php
+++ wp-content/themes/bostonmoves/page-rent.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <div style="display: flex; justify-content: center;">
-<div style="width: 90vw;">
+<div style="width: 90vw; max-width: 1920px;">
 <?php the_content(); ?>
 </div>
 </div>
