7c91fb292d1f89a69a4a86a6614962569a6b7094
Simon Lee <misaeng.dev@gmail.com>
2023-01-25 10:39:32 -0500

    fix: buy rent: screen width changed

--- wp-content/themes/bostonmoves/page-buy.php
+++ wp-content/themes/bostonmoves/page-buy.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 <!-- <h1>THIS IS BUY PAGE.</h1> -->
   <div style="display: flex; justify-content: center;">
-    <div style=" width: 85vw;">
+    <div style="width: 90vw; max-width: 1920px;">
       <?php the_content(); ?>
     </div>
   </div>
